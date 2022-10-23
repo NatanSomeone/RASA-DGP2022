4cb4fd0b6c9e75e2f5e25fcc2be23b5967e6ce17
NatanSomeone <Johnnro1@hotmail.com>
2022-10-23 01:48:47 -0300

    ---datetime

--- DGP-2022/actions/actions.py
+++ DGP-2022/actions/actions.py
@@ -44,8 +44,8 @@ class ActionHelloWorld(Action):
 
         # url = "http://worldtimeapi.org/api/timezone/America/Sao_Paulo";
         # json_data = requests. get(url) .json();
-        # __TIME = datetime.fromtimestamp(json_data["unixtime"]).strftime("%a, %d %b %Y %H:%M:%S");
-        __TIME = datetime.now().strftime("%a, %d %b %Y %H:%M:%S");
+        __TIME = dt.fromtimestamp(json_data["unixtime"]).strftime("%a, %d %b %Y %H:%M:%S");
+        # __TIME = dt.now().strftime("%a, %d %b %Y %H:%M:%S");
         dispatcher.utter_message(text=f"o tempo é {__TIME}")
 
         return []
